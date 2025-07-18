588a6f32fc5730ea3739b05bac4e538489567154
Taylor McKinnon <mail@tmacs.space>
2025-07-17 19:12:56 -0700

    amd cpu temp

--- bin/cpu-temp.sh
+++ bin/cpu-temp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sensors | awk '/Package id 0:/{ print $4 }' | tr -d '+' | cut -c 1-2,5-
+sensors | awk '/AMD AMDSI/{ print $2 }' | tr -d '+' | cut -c 1-2,5-
+#sensors | awk '/Package id 0:/{ print $4 }' | tr -d '+' | cut -c 1-2,5-
